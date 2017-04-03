17278548c3c41d29ab5228cd914655ca7cd4ab3d
sagarsaija <sagarsaija@yahoo.com>
2017-04-03 16:01:29 -0700

    recieved first video's JSON

--- player.js
+++ player.js
@@ -1,20 +1,18 @@
-//AJAX calling API for riipen.mediacore.tv
-
-$(document).ready(function() {
-
+var video_player = function() {
   var video_playlist = [];
   var curr_video = 0;
 
-  $.ajax({
-    method:'GET',
-    url: 'https://riipen.mediacore.tv/api2/media',
-    dataType: 'json',
-    username: "riipenchallenge@mediacore.com", 
-    password: "riipenchallenge",
-    crossDomain: true,
+  function play_video(index) {
+    console.log(video_playlist[index]);
 
-    success: function(videos) {
-      $.each(videos.items, function(key, video){
+    $('#video-player')[0].src = "https://riipen.mediacore.tv/media/id:" + video_playlist[index].id + "/embed_player?iframe=True"
+    $('#title').html(video_playlist[index].title);
+    $('#description').html(video_playlist[index].description);
+  }
+
+  function onSuccess(videos) {
+      for(var index= 0; index < videos.items.length; index++){
+        var video = videos.items[index];
         if (video.type === "video"){
           var video_info = {
             id : video.id,
@@ -23,11 +21,35 @@ $(document).ready(function() {
           };
           video_playlist.push(video_info);
         }
-        console.log(video_playlist);
-      });
-    },
+      };
+           
+      play_video(0);
+      for(var index = 0; index < video_playlist.length; index++) {
+        console.log(video_playlist[index]);
+      }
+  }
+
+  //AJAX calling API for riipen.mediacore.tv
+  function start_videos() {
+    $.ajax({
+    method:'GET',
+    url: 'https://riipen.mediacore.tv/api2/media',
+    dataType: 'json',
+    username: "riipenchallenge@mediacore.com", 
+    password: "riipenchallenge",
+    crossDomain: true,
+    success: onSuccess,
     failure: function (issue) {
-        alert(issue + " error occured");
+          alert(issue + " error occured");
     },
   });
+  }
+
+  return {
+    start : start_videos
+  };
+}();
+
+$(document).ready(function() {
+  video_player.start();
 });
